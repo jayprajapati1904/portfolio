75607a1e14e731d0c6f309222ccb51f14f9521b6
Jay <jay1904prajapati@gmail.com>
2024-11-17 23:27:43 +0530

    add smooth scrolling

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "../src/pages/Home";
+import "./App.css";
 
 const App = () => {
   return (
